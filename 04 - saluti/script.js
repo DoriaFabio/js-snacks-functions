ef7543f91c99f811402f765dadd219eaec327a75
DoriaFabio <doriafabio2001@gmail.com>
2024-11-06 20:22:40 +0100

    do fourth exercise

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -1,13 +1,21 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const name = 'Mario';
-
+const nome = "Mario";
 
 // Dichiara la funzione qui.
+// 1 metodo
+function hello (name) {
+    console.log("Ciao " + name);
+}
+
+// 2 metodo (Arrowfunction)
+let helloArrow = (name) => "Ciao " + name;
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log("1 metodo");
+hello(nome);
+console.log("2 metodo");
+console.log(helloArrow(nome));
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
