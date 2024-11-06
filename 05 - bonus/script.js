f619330e2064dd31a12d3e75761e8dd61ddb5cbb
DoriaFabio <doriafabio2001@gmail.com>
2024-11-06 20:50:07 +0100

    do exercise bonus

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -5,14 +5,25 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
+const nome = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+function hellohour(name) {
+    const date = new Date();
+    console.log(date);
+    const hour = date.getHours();
+    if(hour >= 6 && hour < 13) {
+        return "Buongiorno " + name;
+    } else if (hour >= 13 && hour < 17) {
+        return "Buon pomeriggio " + name;
+    } else if (hour >= 17 || hour < 6) {
+        return "Buonasera " + name;
+    }
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(hellohour(nome));
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
